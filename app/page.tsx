3089a35ee28d7e900fe552462969874961e68feb
djenidisimple <djenidigauss@gmail.com>
2026-03-07 21:14:38 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export function Home() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to the Home Page!</h1>
